796469bc49e4ee031c8e08b9fcb196e926b2c839
Ed Hill <hill.charles2@gmail.com>
2018-09-04 11:53:11 -0400

    initial commit

---
+++ src/content.js
@@ -0,0 +1,30 @@
+function attachBarcodeListeners() {
+    var patronInput = document.getElementsByClassName('barcodeinput barcoderecieved-patron')[0];
+    patronInput.addEventListener('keydown', function (e) {
+        var code = e.which;
+        if (code === 31 || code === 13 || code === 188 || code === 186) {
+            patronInput.value = scrubBarcode(patronInput);
+        }
+    });
+
+    document.querySelector('.patron-barcode-input .barcode-submit').addEventListener('click', function () {
+        patronInput.value = scrubBarcode(patronInput);
+    });
+}
+
+function scrubBarcode(patronInput) {
+    var scrubbedBarcode = patronInput.value.replace(/[;?]/g, '');
+    if (scrubbedBarcode.length > 9) {
+        scrubbedBarcode = scrubbedBarcode.slice(0, 9)
+    }
+    return scrubbedBarcode;
+}
+
+if (window.location.href.indexOf('wms/cmnd/circ/') !== -1) {
+    var interval = setInterval(function () {
+        if (document.getElementsByClassName('barcodeinput barcoderecieved-patron').length > 0) {
+            clearInterval(interval);
+            attachBarcodeListeners();
+        }
+    }, 100);
+}
